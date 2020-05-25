920f3de38d858c501e2dc0a645ec7d7805b1504c
Ricardo Sacardo <rperezsacardo@gmail.com>
2020-05-25 14:29:30 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,43 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <nav>
+            <img src="images/ironhack-logo.svg" alt="" />
+            <img src="images/menu-top.svg" alt="" />
+          </nav>
+          <section className="containeir-1">
+            <div>
+              <h1>Say hello to</h1>
+              <h1>ReactJS</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <button>
+              <a href="/">Awsome!</a>
+            </button>
+          </section>
+          <section className="containeir-2">
+            <div>
+              <img src="images/icon1.png" alt="" />
+              <h3>Title</h3>
+              <p>Nice and short description</p>
+            </div>
+            <div>
+              <img src="images/icon2.png" alt="" />
+              <h3>Title</h3>
+              <p>Nice and short description</p>
+            </div>
+            <div>
+              <img src="images/icon3.png" alt="" />
+              <h3>Title</h3>
+              <p>Nice and short description</p>
+            </div>
+            <div>
+              <img src="images/icon4.png" alt="" />
+              <h3>Title</h3>
+              <p>Nice and short description</p>
+            </div>
+          </section>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
